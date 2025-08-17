0e910617485152a28962aa3ff254845a35a6b579
Marcus Brose <contact@marcusbrose.com>
2025-08-17 14:31:43 +0200

    Basic storyblok page + text component setup

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 const route = useRoute()
+const url = route.path.substring(1) || 'home'
+const { pending, error, story } = await useAsyncStoryblok(url, {
+  api: {
+    version: 'draft', // or 'published'
+  },
+  bridge: {
+    resolveRelations: [],
+    resolveLinks: 'url',
+  }
+})
 
 </script>
 
 <template>
-  <pre>{{ route }}</pre>
+  <div v-if="pending">Loading...</div>
+  <div v-else-if="error">Error: {{ error.message }}</div>
+  <StoryblokComponent v-else :blok="story.content" />
 </template>
---
+++ storyblok/Text.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({ blok: Object })
+const { render } = useStoryblokRichText({
+  // options like resolvers
+})
+
+const root = () => render(props.blok.text)
+</script>
+
+<template>
+  <div class="prose">
+    <root />
+  </div>
+</template>
